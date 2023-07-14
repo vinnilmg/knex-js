36370eb874fbcb32bbcd0b6690bb1f30c4d1e8ee
vinnilmg <vini.delimagomes@gmail.com>
2023-07-14 20:51:31 -0300

    fix

--- knex/queries/raw-query.js
+++ knex/queries/raw-query.js
@@ -2,7 +2,10 @@ const knex = require('../config/database');
 
 const query = `
 INSERT INTO users(first_name, last_name, email, password_hash, salary) 
-VALUES ('teste1', 'teste2', 'myemail4@email.com', '123dasdsad', 10000.0);
+VALUES 
+('Vini', 'Gomes', 'myemail5@email.com', '123_hash', 1200.0),
+('Alpheu', 'TOP', 'myemail10@email.com', '456_hash', 5400.0),
+('Daniboy', 'Lima', 'myemail11@email.com', '789_hash', 1122.0);
 `;
 
 // query RAW
